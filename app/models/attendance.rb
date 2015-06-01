2e338287685feaa81dfba0b69178f7423871e082
Bruno Soares <bruno@bsoares.com.br>
2015-06-01 16:49:45 -0300

    Refactoring date validations of attendance model

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -12,10 +12,10 @@ class Attendance < ActiveRecord::Base
 
   validates :entrance_at,
     presence: true
-  validate :entrance_in_another_day_validation
+  validate :entrance_at_day_validation
 
   validate :departure_less_than_entrance_validation
-  validate :departure_in_another_day_validation
+  validate :departure_at_day_validation
 
   def self.today_attendance(employee)
     Attendance.find_or_initialize_by(employee: employee, day: Date.today)
@@ -31,15 +31,17 @@ class Attendance < ActiveRecord::Base
         ".greater_than_entrance_at"))
   end
 
-  def departure_in_another_day_validation
-    return if departure_at.nil?
-    return if day == departure_at.to_date
-    errors.add(:departure_at, I18n.t("errors.messages.invalid"))
+  def entrance_at_day_validation
+    attendance_date_check(self.entrance_at, :entrance_at)
   end
 
-  def entrance_in_another_day_validation
-    return if entrance_at.nil?
-    return if day == entrance_at.to_date
-    errors.add(:entrance_at, I18n.t("errors.messages.invalid"))
+  def departure_at_day_validation
+    attendance_date_check(self.departure_at, :departure_at)
+  end
+
+  def attendance_date_check(field, symbol)
+    return if field.nil?
+    return if day == field.to_date
+    errors.add(symbol, I18n.t("errors.messages.invalid"))
   end
 end
